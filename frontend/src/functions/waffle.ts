302ff7ca1534536792409a9560a7f273c64f23c8
Juan Ernesto Alonso Tafoya <59717678+juan430@users.noreply.github.com>
2025-09-21 04:29:24 -0600

    Initialize dependabot/pip/drf-spectacular-0.27.2

---
+++ frontend/src/functions/waffle.ts
@@ -0,0 +1,19 @@
+import { FlagNames, RuntimeData } from "../hooks/api/runtimeData";
+
+export type RuntimeDataWithWaffle = RuntimeData & {
+  WAFFLE_FLAGS: RuntimeData["WAFFLE_FLAGS"];
+};
+
+export function isFlagActive(
+  runtimeData: RuntimeData | undefined,
+  flagName: FlagNames,
+): runtimeData is RuntimeDataWithWaffle {
+  if (runtimeData?.WAFFLE_FLAGS) {
+    for (const flag of runtimeData.WAFFLE_FLAGS) {
+      if (flag[0] === flagName && flag[1] === true) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
