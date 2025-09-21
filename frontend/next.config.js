62a693f4bc46ce584ef89ebd8e86f140b30b9a7f
Juan Ernesto Alonso Tafoya <59717678+juan430@users.noreply.github.com>
2025-09-21 04:28:38 -0600

    Initialize v2023.11.08.1

---
+++ frontend/next.config.js
@@ -0,0 +1,132 @@
+/** @type { Record<string, import("./src/config").RuntimeConfig> } */
+const runtimeConfigs = {
+  production: {
+    // The front-end and back-end are served from the same domain in production,
+    // so relative URLs can be used:
+    backendOrigin: "",
+    frontendOrigin: "",
+    fxaLoginUrl: "/accounts/fxa/login/?process=login",
+    fxaLogoutUrl: "/accounts/logout/",
+    supportUrl: "https://support.mozilla.org/products/relay",
+    emailSizeLimitNumber: 10,
+    emailSizeLimitUnit: "MB",
+    maxFreeAliases: 5,
+    mozmailDomain: "mozmail.com",
+    googleAnalyticsId: "UA-77033033-33",
+    maxOnboardingAvailable: 3,
+    maxOnboardingFreeAvailable: 3,
+    featureFlags: {
+      // Also add keys here to RuntimeConfig in src/config.ts
+      tips: true,
+      generateCustomAliasMenu: true,
+      generateCustomAliasSubdomain: false,
+      interviewRecruitment: true,
+      csatSurvey: true,
+    },
+  },
+};
+
+// This configuration is for the setup where the Next.js dev server
+// is running concurrently with the Django server.
+// Due to not running on the same server as the back-end,
+// login and logout needs to be simulated using the `/mock/` pages.
+runtimeConfigs.development = {
+  ...runtimeConfigs.production,
+  backendOrigin: "http://127.0.0.1:8000",
+  frontendOrigin: "http://localhost:3000",
+  fxaLoginUrl: "http://localhost:3000/mock/login",
+  fxaLogoutUrl: "http://localhost:3000/mock/logout",
+};
+
+// This configuration is for the setup where the front-end is built and served
+// on its own, with the back-end mocked out using Mock Service Worker.
+// Login and logout need to be simulated using the `/mock/` pages.
+runtimeConfigs.apimock = {
+  ...runtimeConfigs.production,
+  backendOrigin: "",
+  frontendOrigin: "",
+  fxaLoginUrl: "/mock/login",
+  fxaLogoutUrl: "/mock/logout",
+};
+
+/** @type {(config: import('next').NextConfig) => import('next').NextConfig} */
+const withBundleAnalyzer = (nextConfig) => {
+  if (process.env.ANALYZE !== "true") {
+    return nextConfig;
+  }
+  // This file is only loaded at build time, so we don't care about tree shaking
+  // being made impossible by a `require` call:
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const getAnalyzerWrapper = require("@next/bundle-analyzer");
+  const withAnalyzer = getAnalyzerWrapper({
+    enabled: process.env.ANALYZE === "true",
+  });
+  return withAnalyzer(nextConfig);
+};
+
+let applicableConfig = "production";
+if (process.env.NEXT_PUBLIC_MOCK_API === "true") {
+  applicableConfig = "apimock";
+}
+if (process.env.NODE_ENV === "development") {
+  applicableConfig = "development";
+}
+
+/** @type {import('next').NextConfig} */
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  // This custom value for `pageExtensions` ensures that
+  // test files are not picked up as pages to render by Next.js.
+  // See https://nextjs.org/docs/api-reference/next.config.js/custom-page-extensions
+  pageExtensions: ["page.ts", "page.tsx", "page.js", "page.jsx"],
+  trailingSlash: true,
+  productionBrowserSourceMaps: true,
+  // Unfortunately we cannot use Next.js's built-in dev server,
+  // as the front-end needs to be served from the same origin as the back-end
+  // in order to use authenticated sessions.
+  // Thus, we use `npm run watch` to create a build every time a file in `src/`
+  // changes — but builds are production builds in Next.js by default.
+  // Thus, we cannot use different .env files for different environments
+  // (https://nextjs.org/docs/basic-features/environment-variables),
+  // and use this mechanism instead:
+  publicRuntimeConfig: runtimeConfigs[applicableConfig],
+  images: {
+    // Since we're statically exporting the frontend (i.e. turning it into HTML
+    // as build time, rather than running Next.js on the backend), we can't use
+    // automatic image optimisation. See
+    // https://nextjs.org/docs/messages/export-image-api
+    unoptimized: true,
+  },
+  // See https://nextjs.org/blog/next-13-3#static-export-for-app-router and
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  output: "export",
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.ftl/,
+      type: "asset/source",
+    });
+
+    // msw/browser is not exported when compiling for Node (see
+    // https://github.com/mswjs/msw/blob/461a1885280451ec0837fdce897f49521b8ff260/package.json#L24)
+    // and msw/node is not exported when compiling for the browser (see
+    // https://github.com/mswjs/msw/blob/461a1885280451ec0837fdce897f49521b8ff260/package.json#L17),
+    // so to avoid build errors, we have to prevent Webpack from trying to
+    // traverse them when building.
+    // See https://github.com/mswjs/msw/issues/1801#issuecomment-1793911389
+    if (options.isServer) {
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: "msw/browser", alias: false });
+      } else {
+        config.resolve.alias["msw/browser"] = false;
+      }
+    } else {
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: "msw/node", alias: false });
+      } else {
+        config.resolve.alias["msw/node"] = false;
+      }
+    }
+
+    return config;
+  },
+});
---
+++ frontend/src/functions/filterAliases.test.ts
@@ -0,0 +1,206 @@
+import {
+  AliasData,
+  CustomAliasData,
+  RandomAliasData,
+} from "../hooks/api/aliases";
+import { filterAliases, Filters } from "./filterAliases";
+
+const getMockedAliasMatching = (
+  filters: Partial<Filters>,
+  customSubdomain = "arbitrary_subdomain",
+): AliasData => {
+  const alias = {
+    address: filters.string ?? "arbitrary_string",
+    full_address:
+      filters.domainType === "custom"
+        ? `${
+            filters.string ?? "arbitrary_string"
+          }@${customSubdomain}.mozmail.com`
+        : `${filters.string ?? "arbitrary_string"}@mozmail.com`,
+    created_at: "2021-11-08T13:46:40.899003Z",
+    description: "",
+    domain: 2,
+    block_list_emails: filters.status === "promo-blocking",
+    enabled: filters.status !== "blocking",
+    id: 42,
+    last_modified_at: "2021-11-08T13:46:40.899003Z",
+    last_used_at: null,
+    num_blocked: 0,
+    num_forwarded: 0,
+    num_spam: 0,
+    mask_type: "custom",
+  } as CustomAliasData | RandomAliasData;
+  if (filters.domainType === "random") {
+    (alias as RandomAliasData).generated_for = "";
+    (alias as RandomAliasData).mask_type = "random";
+  }
+  return alias;
+};
+const getOneOfEvery = (): AliasData[] => {
+  return [
+    getMockedAliasMatching({
+      string: "some_string",
+      domainType: "custom",
+      status: "blocking",
+    }),
+    getMockedAliasMatching({
+      string: "other_string",
+      domainType: "custom",
+      status: "blocking",
+    }),
+    getMockedAliasMatching({
+      string: "some_string",
+      domainType: "random",
+      status: "blocking",
+    }),
+    getMockedAliasMatching({
+      string: "other_string",
+      domainType: "random",
+      status: "blocking",
+    }),
+    getMockedAliasMatching({
+      string: "some_string",
+      domainType: "custom",
+      status: "promo-blocking",
+    }),
+    getMockedAliasMatching({
+      string: "other_string",
+      domainType: "custom",
+      status: "promo-blocking",
+    }),
+    getMockedAliasMatching({
+      string: "some_string",
+      domainType: "random",
+      status: "promo-blocking",
+    }),
+    getMockedAliasMatching({
+      string: "other_string",
+      domainType: "random",
+      status: "promo-blocking",
+    }),
+    getMockedAliasMatching({
+      string: "some_string",
+      domainType: "custom",
+      status: "forwarding",
+    }),
+    getMockedAliasMatching({
+      string: "other_string",
+      domainType: "custom",
+      status: "forwarding",
+    }),
+    getMockedAliasMatching({
+      string: "some_string",
+      domainType: "random",
+      status: "forwarding",
+    }),
+    getMockedAliasMatching({
+      string: "other_string",
+      domainType: "random",
+      status: "forwarding",
+    }),
+  ];
+};
+
+it("returns all aliases if no filters are active", () => {
+  const aliases = getOneOfEvery();
+
+  expect(filterAliases(aliases, { string: "" })).toStrictEqual(aliases);
+});
+
+it("filters out aliases that do not match the given string", () => {
+  const aliases = [
+    getMockedAliasMatching({ string: "some_string" }),
+    getMockedAliasMatching({ string: "other_string" }),
+  ];
+
+  expect(filterAliases(aliases, { string: "some" })).toStrictEqual([
+    aliases[0],
+  ]);
+});
+
+it("can match on an alias's subdomain when using a string filter", () => {
+  const aliases = [
+    getMockedAliasMatching({ domainType: "random" }),
+    getMockedAliasMatching({ domainType: "custom" }, "some_subdomain"),
+  ];
+
+  expect(filterAliases(aliases, { string: "some" })).toStrictEqual([
+    aliases[1],
+  ]);
+});
+
+it("can filter out non-random domains", () => {
+  const aliases = [
+    getMockedAliasMatching({ domainType: "random" }),
+    getMockedAliasMatching({ domainType: "custom" }),
+  ];
+
+  expect(
+    filterAliases(aliases, { string: "", domainType: "random" }),
+  ).toStrictEqual([aliases[0]]);
+});
+
+it("can filter out non-random domains", () => {
+  const aliases = [
+    getMockedAliasMatching({ domainType: "random" }),
+    getMockedAliasMatching({ domainType: "custom" }),
+  ];
+
+  expect(
+    filterAliases(aliases, { string: "", domainType: "custom" }),
+  ).toStrictEqual([aliases[1]]);
+});
+
+it("can filter out blocking aliases", () => {
+  const aliases = [
+    getMockedAliasMatching({ status: "blocking" }),
+    getMockedAliasMatching({ status: "promo-blocking" }),
+    getMockedAliasMatching({ status: "forwarding" }),
+  ];
+
+  expect(
+    filterAliases(aliases, { string: "", status: "forwarding" }),
+  ).toStrictEqual([aliases[2]]);
+});
+
+it("can filter out aliases that are blocking promotional emails", () => {
+  const aliases = [
+    getMockedAliasMatching({ status: "blocking" }),
+    getMockedAliasMatching({ status: "promo-blocking" }),
+    getMockedAliasMatching({ status: "forwarding" }),
+  ];
+
+  expect(
+    filterAliases(aliases, { string: "", status: "promo-blocking" }),
+  ).toStrictEqual([aliases[1]]);
+});
+
+it("can filter out forwarding aliases", () => {
+  const aliases = [
+    getMockedAliasMatching({ status: "blocking" }),
+    getMockedAliasMatching({ status: "promo-blocking" }),
+    getMockedAliasMatching({ status: "forwarding" }),
+  ];
+
+  expect(
+    filterAliases(aliases, { string: "", status: "blocking" }),
+  ).toStrictEqual([aliases[0]]);
+});
+
+it("can combine filters", () => {
+  const aliases = [
+    getMockedAliasMatching({ string: "some_string", status: "blocking" }),
+    getMockedAliasMatching({ string: "some_string", status: "promo-blocking" }),
+    getMockedAliasMatching({ string: "some_string", status: "forwarding" }),
+    getMockedAliasMatching({ string: "other_string", status: "blocking" }),
+    getMockedAliasMatching({
+      string: "other_string",
+      status: "promo-blocking",
+    }),
+    getMockedAliasMatching({ string: "other_string", status: "forwarding" }),
+  ];
+
+  expect(
+    filterAliases(aliases, { string: "some", status: "forwarding" }),
+  ).toStrictEqual([aliases[2]]);
+});
