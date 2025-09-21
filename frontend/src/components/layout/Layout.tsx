c9325bd42da646906e307c25993f1af30aa8865b
Juan Ernesto Alonso Tafoya <59717678+juan430@users.noreply.github.com>
2025-09-21 04:27:28 -0600

    Initialize wrong-img-news

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,213 @@
+import { ReactNode } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useLocalization } from "@fluent/react";
+import { ToastContainer, toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styles from "./Layout.module.scss";
+import logoTypeLight from "../../../../static/images/fx-private-relay-logotype-light.svg";
+import logoTypeDark from "../../../../static/images/fx-private-relay-logotype-dark.svg";
+import logoTypePremiumLight from "../../../../static/images/fx-private-relay-premium-logotype-light.svg";
+import logoTypePremiumDark from "../../../../static/images/fx-private-relay-premium-logotype-dark.svg";
+import logo from "../../../../static/images/placeholder-logo.svg";
+import mozillaLogo from "../../../../static/images/logos/moz-logo-bw-rgb.svg";
+import favicon from "../../../public/favicon.svg";
+import socialMediaImage from "../../../../static/images/share-relay.jpg";
+import { useProfiles } from "../../hooks/api/profile";
+import { UserMenu } from "./UserMenu";
+import { Navigation } from "./Navigation";
+import { AppPicker } from "./AppPicker";
+import { useIsLoggedIn } from "../../hooks/session";
+import { NpsSurvey } from "./NpsSurvey";
+import { getRuntimeConfig } from "../../config";
+import { CsatSurvey } from "./CsatSurvey";
+import { InterviewRecruitment } from "./InterviewRecruitment";
+import { WhatsNewMenu } from "./whatsnew/WhatsNewMenu";
+
+export type Props = {
+  children: ReactNode;
+  theme?: "free" | "premium";
+};
+
+/**
+ * Standard page layout for Relay, wrapping its children in the relevant header and footer.
+ */
+export const Layout = (props: Props) => {
+  const { l10n } = useLocalization();
+  const profiles = useProfiles();
+  const isLoggedIn = useIsLoggedIn();
+  const router = useRouter();
+
+  const isDark =
+    typeof props.theme !== "undefined"
+      ? props.theme === "free"
+      : !profiles.data?.[0].has_premium;
+  const darkClass = isDark ? styles["is-dark"] : styles["is-light"];
+  const premiumLogo = isDark ? logoTypePremiumLight : logoTypePremiumDark;
+  const regularLogo = isDark ? logoTypeLight : logoTypeDark;
+  // The Premium logo is always shown if the user has Premium
+  // (even on a page accessible to regular users with the theme
+  // set to "free", like the FAQ), and if the theme is explicitly
+  // set to Premium (i.e. on the `/premium` promo page).
+  const logoType =
+    props.theme === "premium" || profiles.data?.[0].has_premium
+      ? premiumLogo
+      : regularLogo;
+  const logoAlt =
+    props.theme === "premium" || profiles.data?.[0].has_premium
+      ? l10n.getString("logo-alt")
+      : l10n.getString("logo-premium-alt");
+
+  const homePath = isLoggedIn ? "/accounts/profile" : "/";
+
+  const csatSurvey =
+    getRuntimeConfig().featureFlags.csatSurvey &&
+    !getRuntimeConfig().featureFlags.interviewRecruitment &&
+    profiles.data?.[0] ? (
+      <CsatSurvey profile={profiles.data[0]} />
+    ) : null;
+  const npsSurvey =
+    !getRuntimeConfig().featureFlags.csatSurvey &&
+    !getRuntimeConfig().featureFlags.interviewRecruitment ? (
+      <NpsSurvey />
+    ) : null;
+
+  const apiMockWarning =
+    process.env.NEXT_PUBLIC_MOCK_API === "true" ? (
+      <div className={styles["api-mock-warning"]}>
+        This is a site to demo the Relay UI; data is fake, and changes will be
+        lost after a page refresh.
+      </div>
+    ) : null;
+
+  const whatsNew =
+    isLoggedIn && profiles.data ? (
+      <WhatsNewMenu profile={profiles.data[0]} />
+    ) : null;
+
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href={favicon.src}></link>
+        <title>{l10n.getString("meta-title")}</title>
+        <meta name="description" content={l10n.getString("meta-description")} />
+        <meta
+          property="og:url"
+          content={getRuntimeConfig().frontendOrigin + router.asPath}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={l10n.getString("meta-title")} />
+        <meta
+          property="og:description"
+          content={l10n.getString("meta-description")}
+        />
+        <meta property="og:image" content={socialMediaImage.src} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@firefox" />
+        <meta name="twitter:title" content={l10n.getString("meta-title")} />
+        <meta
+          name="twitter:description"
+          content={l10n.getString("meta-description")}
+        />
+        <meta name="twitter:image" content={socialMediaImage.src} />
+      </Head>
+      <div className={styles.wrapper}>
+        {apiMockWarning}
+        <InterviewRecruitment profile={profiles.data?.[0]} />
+        {csatSurvey}
+        {npsSurvey}
+        <header className={`${styles.header} ${darkClass}`}>
+          <div className={styles["logo-wrapper"]}>
+            <Link href={homePath}>
+              <a className={styles.logo}>
+                <img
+                  src={logo.src}
+                  alt=""
+                  className={styles.logomark}
+                  width={42}
+                />
+                <img
+                  src={logoType.src}
+                  alt={logoAlt}
+                  className={styles.logotype}
+                  height={20}
+                />
+              </a>
+            </Link>
+          </div>
+          <div className={styles["nav-wrapper"]}>
+            <Navigation />
+            {whatsNew}
+          </div>
+          <div className={styles["app-picker-wrapper"]}>
+            <AppPicker theme={isDark ? "free" : "premium"} />
+          </div>
+          <nav className={styles["user-menu-wrapper"]}>
+            <UserMenu />
+          </nav>
+        </header>
+        <ToastContainer
+          position={toast.POSITION.TOP_CENTER}
+          theme="colored"
+          transition={Slide}
+          autoClose={5000}
+          toastClassName={`Toastify__toast ${styles.toast}`}
+        />
+        <div className={styles.content}>{props.children}</div>
+        <footer className={styles.footer}>
+          <a
+            href="https://www.mozilla.org"
+            className={styles["mozilla-logo"]}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={mozillaLogo.src}
+              alt={l10n.getString("logo-mozilla-alt")}
+              width={120}
+            />
+          </a>
+          <ul className={styles.meta}>
+            <li>
+              <a
+                href="https://www.mozilla.org/privacy/firefox-relay/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {l10n.getString("nav-footer-privacy")}
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.mozilla.org/about/legal/terms/firefox-relay/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {l10n.getString("nav-footer-relay-terms")}
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.mozilla.org/about/legal/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {l10n.getString("nav-footer-legal")}
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/mozilla/fx-private-relay"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </footer>
+      </div>
+    </>
+  );
+};
---
+++ frontend/src/hooks/api/aliases.ts
@@ -0,0 +1,185 @@
+import { SWRResponse } from "swr";
+import { apiFetch, useApiV1 } from "./api";
+import { ProfileData } from "./profile";
+
+type DateTimeString = string;
+type Domain_RelayFirefoxCom = 1;
+type Domain_MozmailCom = 2;
+export type CommonAliasData = {
+  type: "random" | "custom";
+  enabled: boolean;
+  block_list_emails: boolean;
+  description: string | "";
+  id: number;
+  address: string;
+  full_address: string;
+  domain: Domain_MozmailCom | Domain_RelayFirefoxCom;
+  created_at: DateTimeString;
+  last_modified_at: DateTimeString;
+  last_used_at: DateTimeString | null;
+  num_forwarded: number;
+  num_blocked: number;
+  num_spam: number;
+};
+
+export type RandomAliasData = CommonAliasData & {
+  type: "random";
+  generated_for: string | "";
+  domain: Domain_RelayFirefoxCom;
+};
+export type CustomAliasData = CommonAliasData & {
+  type: "custom";
+  domain: Domain_MozmailCom;
+};
+
+export type AliasData = RandomAliasData | CustomAliasData;
+
+export type AliasCreateFn = (
+  options: { type: "random" } | { type: "custom"; address: string }
+) => Promise<Response>;
+export type AliasUpdateFn = (
+  alias: Pick<CommonAliasData, "id" | "type">,
+  updatedFields: Partial<AliasData>
+) => Promise<Response>;
+export type AliasDeleteFn = (alias: AliasData) => Promise<Response>;
+
+/**
+ * Fetch aliases (both random and custom) from our API using [SWR](https://swr.vercel.app).
+ */
+export function useAliases(): {
+  randomAliasData: SWRResponse<RandomAliasData[], unknown>;
+  customAliasData: SWRResponse<CustomAliasData[], unknown>;
+  create: AliasCreateFn;
+  update: AliasUpdateFn;
+  delete: AliasDeleteFn;
+} {
+  const randomAliases: SWRResponse<RandomAliasData[], unknown> =
+    useApiV1("/relayaddresses/");
+  const customAliases: SWRResponse<CustomAliasData[], unknown> =
+    useApiV1("/domainaddresses/");
+
+  const randomAliasData = {
+    ...randomAliases,
+    data: randomAliases.data?.map((alias) => markAsRandomAlias(alias)),
+  };
+  const customAliasData = {
+    ...customAliases,
+    data: customAliases.data?.map((alias) => markAsCustomAlias(alias)),
+  };
+
+  const createAlias: AliasCreateFn = async (options) => {
+    if (options.type === "custom") {
+      const response = await apiFetch("/domainaddresses/", {
+        method: "POST",
+        body: JSON.stringify({ enabled: true, address: options.address }),
+      });
+      customAliases.mutate();
+      return response;
+    }
+
+    const response = await apiFetch("/relayaddresses/", {
+      method: "POST",
+      body: JSON.stringify({ enabled: true }),
+    });
+    randomAliases.mutate();
+    return response;
+  };
+
+  const updateAlias: AliasUpdateFn = async (alias, updatedFields) => {
+    const endpoint =
+      alias.type === "random"
+        ? `/relayaddresses/${alias.id}/`
+        : `/domainaddresses/${alias.id}/`;
+
+    const response = await apiFetch(endpoint, {
+      method: "PATCH",
+      body: JSON.stringify(updatedFields),
+    });
+    if (alias.type === "random") {
+      randomAliases.mutate();
+    } else {
+      customAliases.mutate();
+    }
+    return response;
+  };
+
+  const deleteAlias: AliasDeleteFn = async (aliasData) => {
+    const endpoint =
+      aliasData.type === "random"
+        ? `/relayaddresses/${aliasData.id}/`
+        : `/domainaddresses/${aliasData.id}/`;
+
+    const response = await apiFetch(endpoint, {
+      method: "DELETE",
+    });
+    if (aliasData.type === "random") {
+      randomAliases.mutate();
+    } else {
+      customAliases.mutate();
+    }
+    return response;
+  };
+
+  return {
+    randomAliasData: randomAliasData,
+    customAliasData: customAliasData,
+    create: createAlias,
+    update: updateAlias,
+    delete: deleteAlias,
+  };
+}
+
+export function isRandomAlias(alias: AliasData): alias is RandomAliasData {
+  return alias.type === "random";
+}
+
+export function getAllAliases(
+  randomAliases: RandomAliasData[],
+  customAliases: CustomAliasData[]
+): AliasData[] {
+  return (randomAliases as AliasData[]).concat(customAliases);
+}
+
+export function getFullAddress(alias: AliasData) {
+  return alias.full_address;
+}
+
+/**
+ * Make an alias trackable as a random alias
+ *
+ * There is nothing in the properties of an alias that will always distinguish
+ * random aliases from custom aliases. The only certain indication we have of
+ * an alias's type, is whether we fetched it from the Random or Custom Alias
+ * endpoint. Thus, we immediately tack on a `type` property after fetching
+ * (using this function) so that we can pass it on to the rest of the app and
+ * still determine the type of the alias.
+ *
+ * @param randomAliasData A random alias fetched from the API
+ * @returns The same alias, but with a `type` property set to `"random"`
+ */
+function markAsRandomAlias(randomAliasData: RandomAliasData): RandomAliasData {
+  return {
+    ...randomAliasData,
+    type: "random",
+  };
+}
+
+/**
+ * Make an alias trackable as a custom alias
+ *
+ * There is nothing in the properties of an alias that will always distinguish
+ * random aliases from custom aliases. The only certain indication we have of
+ * an alias's type, is whether we fetched it from the Random or Custom Alias
+ * endpoint. Thus, we immediately tack on a `type` property after fetching
+ * (using this function) so that we can pass it on to the rest of the app and
+ * still determine the type of the alias.
+ *
+ * @param randomAliasData A custom alias fetched from the API
+ * @returns The same alias, but with a `type` property set to `"custom"`
+ */
+function markAsCustomAlias(customAliasData: CustomAliasData): CustomAliasData {
+  return {
+    ...customAliasData,
+    type: "custom",
+  };
+}
